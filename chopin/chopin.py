396202b59cb9944c828f1a471c20b8c849917933
ster <ster.physics@gmail.com>
2021-05-13 18:33:11 +0900

    修正：インスタンス生成時の出力を変更できるように修正

--- chopin/chopin.py
+++ chopin/chopin.py
@@ -11,22 +11,17 @@ import requests
 
 class Link():
     """
-    ```
     - Link.url
     - Link.ID
     - Link.artists
     - Link.download(path=None)
     - Link.delete()
-    ```
     """
     def __init__(self,url,ID,artists):
         self.url = url
         self.ID = ID
         self.artists = artists.split(",")
         self._path = "/tmp/{}.mp3".format(ID)
-
-    def __repr__(self):
-        return "Link()"
 
     def __str__(self):
         s = "{"
@@ -53,23 +48,18 @@ class Link():
 
 class Composition():
     """
-    ```
     - Composition.title
     - Composition.Opus
     - Composition.No
     - Composition.links -> [Link()]
     - Composition.wikiLink
-    ```
     """
     def __init__(self,title,Op,No,wikiLink):
         self.title = title
         self.Opus = None if Op == "ㅤ" else Op
         self.No = None if No == "ㅤ" else No
-        self.links = []
+        self.links: list(Link) = []
         self.wikiLink = wikiLink
-
-    def __repr__(self):
-        return "Composition()"
 
     def __str__(self):
         s = "{} ".format(self.title)
@@ -84,12 +74,14 @@ class Composition():
         self.links.append(link)
 
 class chopin():
-    def __init__(self, parallel=False, semaphore=5):
+    def __init__(self, parallel=False, semaphore=5, output=True):
         self.compositionList = []
-        if parallel:
-            self.compositionList = Init().mainPara(semaphore)
-        else:
-            self.compositionList = Init().mainNotPara()
+        while not self.compositionList:
+            init = Init(output)
+            if parallel:
+                self.compositionList = init.mainPara(semaphore)
+            else:
+                self.compositionList = init.mainNotPara()
         self.genreList = [
             "Ballade",
             "Bolero",
@@ -151,18 +143,19 @@ class chopin():
         return outputCompo
 
 class Init():
+    def __init__(self, output=True):
+        self._wiki = Wiki(output)
+
     def mainNotPara(self) -> list:
-        wiki = Wiki()
-        wiki.mainNotPara()
-        return wiki.compoList
+        self._wiki.mainNotPara()
+        return self._wiki.compoList
 
     def mainPara(self,semaphore) -> list:
-        wiki = Wiki()
-        wiki.mainPara(semaphore)
-        return wiki.compoList
+        self._wiki.mainPara(semaphore)
+        return self._wiki.compoList
 
 class Wiki():
-    def __init__(self):
+    def __init__(self,output=True):
         self._url = "https://github.com/ster-phys/chopin/wiki"
         text = requests.get(self._url).text
         ptn = r'<tr>\n<td align="center"><a href="(?P<link>.*?)"><img class="emoji" title=":link:" alt=":link:" src="https://camo.githubusercontent.com/fa7d9823f78ebcc07a20dc50a4c7c90d8c48cdee5f2dcacb7cd458e420ac64e8/68747470733a2f2f6769746875622e6769746875626173736574732e636f6d2f696d616765732f69636f6e732f656d6f6a692f756e69636f64652f31663531372e706e67" height="20" width="20" align="absmiddle" data-canonical-src="https://github.githubassets.com/images/icons/emoji/unicode/1f517.png"></a></td>\n<td align="left">(?P<title>.*?)</td>\n<td align="center">(?P<Opus>.*?)</td>\n<td align="center">(?P<No>.*?)</td>\n</tr>'
@@ -170,13 +163,16 @@ class Wiki():
         for res in re.findall(ptn,text):
             compo = Composition(res[1],res[2],res[3],res[0])
             self.compoList.append(compo)
+        self.output = output
 
     def req(self,i:int):
         ptn = r'<tr>\n<td align="center"><a href="(?P<link>.*?)" rel="nofollow"><img class="emoji" title=":link:" alt=":link:" src="https://camo.githubusercontent.com/fa7d9823f78ebcc07a20dc50a4c7c90d8c48cdee5f2dcacb7cd458e420ac64e8/68747470733a2f2f6769746875622e6769746875626173736574732e636f6d2f696d616765732f69636f6e732f656d6f6a692f756e69636f64652f31663531372e706e67" height="20" width="20" align="absmiddle" data-canonical-src="https://github.githubassets.com/images/icons/emoji/unicode/1f517.png"></a></td>\n<td align="center"><code>(?P<ID>.*?)/code></td>\n<td align="left">(?P<artists>.*?)</td>\n</tr>'
-        print("{:>3}/{} Connect to {}".format(i+1,len(self.compoList),self.compoList[i].wikiLink))
+        if self.output:
+            print("{:>3}/{} Connect to {}".format(i+1,len(self.compoList),self.compoList[i].wikiLink))
         text = requests.get(self.compoList[i].wikiLink).text
         resList = re.findall(ptn,text)
-        # print("Found {} IDs".format(len(resList)))
+        if self.output:
+            print("Found {} IDs".format(len(resList)))
         for res in resList:
             self.compoList[i]._appendLink(res[0], res[1], res[2])
 
