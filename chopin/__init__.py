ad1cad7cd917457f7d16f4fffdf5cd7d30a8a033
ster <ster.physics@gmail.com>
2022-07-19 02:31:37 +0900

    Version bump to v0.2.0

--- chopin/__init__.py
+++ chopin/__init__.py
@@ -1,8 +1,6 @@
-# -*- coding: utf-8 -*-
-
 """
-Chopin's Compositions
-~~~~~~~~~~~~~~~~~~~~~
+Chopin Compositions
+~~~~~~~~~~~~~~~~~~~
 
 :copyright: (c) 2021-present ster
 :license: Mit, see LICENSE for more details.
@@ -13,10 +11,14 @@ __title__ = "chopin"
 __author__ = "ster"
 __license__ = "MIT"
 __copyright__ = "Copyright 2021-present ster"
-__version__ = "0.2.0a"
+__version__ = "0.2.0"
 
-from .chopin import *
+from .chopin import Chopin
+from .compositions import Composition
+from .contents import Content
 
 __all__ = (
     "Chopin",
+    "Composition",
+    "Content",
 )
