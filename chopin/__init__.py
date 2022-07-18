6d7797e80bae2ff37c68b6b1f70fd9d8be6e2d81
ster <ster.physics@gmail.com>
2022-07-18 23:04:23 +0900

    Version bump to v0.2.0a

--- chopin/__init__.py
+++ chopin/__init__.py
@@ -13,7 +13,7 @@ __title__ = "chopin"
 __author__ = "ster"
 __license__ = "MIT"
 __copyright__ = "Copyright 2021-present ster"
-__version__ = "0.1.1"
+__version__ = "0.2.0a"
 
 from .chopin import *
 
